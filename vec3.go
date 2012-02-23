66cf4b8221a168fc6a3ec69b836bf85c69b3a518
Robert Wertman <robert.wertman@gmail.com>
2012-02-23 11:10:54 -0500

    Floating point is weird. This is probably faster.

--- vec3.go
+++ vec3.go
@@ -3,9 +3,5 @@ package main
 import "math"
 
 func (v vec3) Length() float64 {
-	size := v[0] * v[0]
-	size += v[1] * v[1]
-	size += v[2] * v[2]
-	size = math.Sqrt(size)
-	return size
+	return math.Sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
 }
