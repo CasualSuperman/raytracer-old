85451a315ed8e3943268f46d7488d7968cf1c700
Robert Wertman <robert.wertman@gmail.com>
2012-03-29 16:06:59 -0400

    Working on shapes.

--- shapes/plane.go
+++ shapes/plane.go
@@ -1,1 +1,15 @@
 package shapes
+
+import "raytracer/vector"
+
+type sphere struct {
+	center vector.Position
+	radius float64
+}
+
+func (s *Sphere) Intersect(r vector.Ray) (hit bool, length float64) {
+}
+
+func String() string {
+	
+}
--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,8 +1,27 @@
 package shapes
 
-import "raytracer/ray"
 import "raytracer/vector"
 
 type Intersecter interface {
-	func Intersect(ray.Ray r) (vector.Vec3, bool)
+	Intersect(r vector.Ray) (bool, float64)
+}
+
+type ShapeId byte
+
+type Shape struct {
+	Next *Shape
+	Id   int
+	Type ShapeId
+
+	Mat Material
+
+	shape Intersecter
+}
+
+type Material struct {
+	Ambient, Diffuse, Specular [3]float64
+}
+
+func (s *Shape) Intersect(r vector.Ray) (bool, float64) {
+	return s.shape.Intersect(r)
 }
