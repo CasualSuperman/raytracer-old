cec8b811c638881d320015b7be35fb02e8e5e28e
Robert Wertman <robert.wertman@gmail.com>
2012-04-28 14:02:08 -0400

    Commented shapes.go

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -13,18 +13,24 @@ import (
 type shapeReader func(*bufio.Reader) (Shape, error)
 type lightReader func(*bufio.Reader) (Light, error)
 
-// A list of types that we know how to read in.
+// A map of types that we know how to read in.
 var shapes = make(map[shapeId]shapeReader)
 var lights = make(map[shapeId]lightReader)
 
-// Shapes can be intersected
+// The shape interface.
 type Shape interface {
-	Specular(*vector.Position) vector.Vec3
-	Ambient(*vector.Position) vector.Vec3
-	Diffuse(*vector.Position) vector.Vec3
-	Intersector
+	// Returns the shape's global ID.
 	Id() int
+	// Return the type of shape that it is.
 	Type() shapeId
+	// Embed the Intersector interface.
+	Intersector
+	// Returns the specular at the given point.
+	Specular(*vector.Position) vector.Vec3
+	// Returns the ambient at the given point.
+	Ambient(*vector.Position) vector.Vec3
+	// Returns the diffuse at the given point.
+	Diffuse(*vector.Position) vector.Vec3
 }
 
 // Things that can be intersected will have these methods.
@@ -35,33 +41,37 @@ type Intersector interface {
 	Hits(r vector.Ray) (bool, float64, *vector.Ray)
 }
 
-// An alias of bytes to shapeId, but with actual type safety
+// An alias of int as shapeId, but with actual type safety
 type shapeId int
 
+// The basic information all shapes have.
 type shape struct {
 	// The global shape id.
 	id int
-	// The shape type.
-	Type shapeId
 	// The shape's material.
 	Mat Material
-	// Where we were last hit.
-	Hit vector.Position
 }
 
-// Register a format with our list of readable formats.
+// Register a given shapeId with our list of shape formats we understand. It
+// will be read in using the provided shapeReader.
 func RegisterShapeFormat(id shapeId, reader shapeReader) {
 	shapes[id] = reader
 }
 
+// Register a given shapeId with our list of light formats we understand. It
+// will be read in using the provided lightReader.
 func RegisterLightFormat(id shapeId, reader lightReader) {
 	lights[id] = reader
 }
 
+// Read in shapes and lights from the given input stream, adding them to the
+// provded lights and shapes slices. Will stop if any format is not understood
+// or a reader returns an error, passing that error back up.
 func Read(r *bufio.Reader, s *[]Shape, l *[]Light) (err error) {
 	err = nil
 	scanning := true
 
+	// While we're trying to scan.
 	for scanning {
 		count, num, line := 0, 0, []byte{}
 		// Read lines until we hit a line that works.
@@ -72,37 +82,7 @@ func Read(r *bufio.Reader, s *[]Shape, l *[]Light) (err error) {
 			}
 		}
 
-		if err == nil {
-			// We can continue.
-			shapeReader, shapeExists := shapes[shapeId(num)]
-			lightReader, lightExists := lights[shapeId(num)]
-
-			if !shapeExists && !lightExists {
-				if debug.SHAPES {
-					log.Printf("id, readCount, shapes, lights: %v, %v, %v,\n%v\n", num, count, shapes, lights)
-				}
-				return fmt.Errorf("Unknown type id %d.", num)
-			}
-
-			if shapeExists {
-				shape, err := shapeReader(r)
-
-				if err != nil {
-					return err
-				}
-
-				*s = append(*s, shape)
-			} else if lightExists {
-				light, err := lightReader(r)
-
-				if err != nil {
-					return err
-				}
-
-				*l = append(*l, light)
-			}
-		} else {
-			// We ran into an error.
+		if err != nil {
 			if err != io.EOF {
 				// Some other error.
 				return fmt.Errorf("Unable to read shape id.")
@@ -110,9 +90,52 @@ func Read(r *bufio.Reader, s *[]Shape, l *[]Light) (err error) {
 				// We're done here.
 				scanning = false
 			}
+		} else {
+			// We can continue. See if we have a compatible reader with the
+			// shapeId we got.
+			shapeReader, shapeExists := shapes[shapeId(num)]
+			lightReader, lightExists := lights[shapeId(num)]
+
+			// If we don't have one, let them know the shape is unsupported.
+			if !shapeExists && !lightExists {
+				if debug.SHAPES {
+					log.Printf("id, read, shapes, lights: %v, %v, %v, %v\n",
+						num, count, shapes, lights)
+				}
+				return fmt.Errorf("Unknown type id %d.", num)
+			}
+
+			// If we have a compatible shapeReader
+			if shapeExists {
+				// Give it our stream and get back the shape.
+				shape, err := shapeReader(r)
+
+				// If there was an error, send it up.
+				if err != nil {
+					return err
+				}
+
+				// Otherwise, append it to our list of shapes.
+				*s = append(*s, shape)
+
+				// If we have a lightReader with the right Id.
+			} else if lightExists {
+				// Give it our stream and get back the light.
+				light, err := lightReader(r)
+
+				// Quit if we got an error.
+				if err != nil {
+					return err
+				}
+
+				// Otherwise, add it to the lights.
+				*l = append(*l, light)
+			}
 		}
+		// Now loop back to finding an input ID if we 're still scanning.
 	}
 
+	// If we broke out of the loop, we finished cleanly.
 	return nil
 }
 
@@ -124,6 +147,7 @@ func (s shape) String() string {
 	return fmt.Sprintf("id: %d\n\tMaterial: \n%s", s.id, s.Mat.String())
 }
 
+// Return our id.
 func (s shape) Id() int {
 	return s.id
 }
@@ -140,8 +164,13 @@ func (s *shape) Read(r *bufio.Reader) error {
 	// Read in our material.
 	err := s.Mat.Read(r)
 	if debug.SHAPES {
-		log.Println("Read in material.")
+		if err == nil {
+			log.Println("Read in material.")
+		} else {
+			log.Println("Error reading in material.")
+		}
 	}
 
+	// Return our error, if we have one.
 	return err
 }
